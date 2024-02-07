4b753e7f35fc80a9f375931c6a76e876389aa155
Nurbek0vM <Nurbekov.mairambek5@mail.ru>
2024-02-07 23:18:54 +0600

    second commit

---
+++ product/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from product import views
+
+urlpatterns = [
+#     path('hello/', views.hello_view),
+#     path('current_date/', views.current_date_view),
+#     path('goodbye/', views.goodbye_view),
+    path('', views.main_view),
+    path('products/', views.product_view),
+
+]
